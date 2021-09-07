401a1bfea15a60eff7dbd2b78af32e1d2f324c4a
lwn-ysy <631955411@qq.com>
2021-09-07 13:04:38 +0800

    设置允许跨域

--- app.js
+++ app.js
@@ -23,7 +23,14 @@ const option = {
 };
 const httpsServer = https.createServer(option, app);
 
-
+// 设置跨域允许
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    next();
+});
 
 // 静态文件路由
 app.use('/static', express.static('static', {
